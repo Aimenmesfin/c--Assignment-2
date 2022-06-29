bb9481747db653ed9bf08a20cdb002fd53407d67
Emanesk19 <101937600+Emanesk19@users.noreply.github.com>
2022-06-29 23:00:24 +0300

    uploaded

---
+++ assignment2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+int[] nums = new int[10];
+int contN = 0, countP = 0, Count = 0;
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    Console.WriteLine("Enter number " + (i + 1));
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    if (nums[i] < 0)
+    {
+        contN++;
+    }
+    else if (nums[i] > 0)
+    {
+        countP++;
+    }
+    else
+    {
+        Count++;
+    }
+}
+Console.WriteLine("Number of negatives : " + contN);
+Console.WriteLine("Number of Posetives : " + countP);
+Console.WriteLine("Number of Zeros : " + Count);
+
